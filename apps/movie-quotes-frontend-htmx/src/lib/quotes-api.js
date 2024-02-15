39359bba997dddb75b2004453e7beca9c8f54bfa
Jonas Galvez <jonasgalvez@gmail.com>
2024-02-15 02:55:02 -0300

    add apps/movie-quotes-frontend-htmx

---
+++ apps/movie-quotes-frontend-htmx/src/lib/quotes-api.js
@@ -0,0 +1,82 @@
+import { createClient, gql, cacheExchange, fetchExchange } from '@urql/core'
+
+const graphqlClient = createClient({
+  url: import.meta.env.PUBLIC_GRAPHQL_API_ENDPOINT,
+  requestPolicy: 'network-only',
+  exchanges: [cacheExchange, fetchExchange]
+})
+
+async function graphqlClientWrapper (method, gqlQuery, queryVariables = {}) {
+  const queryResult = await graphqlClient[method](
+    gqlQuery,
+    queryVariables
+  ).toPromise()
+
+  if (queryResult.error) {
+    console.error('GraphQL error:', queryResult.error)
+  }
+
+  return {
+    data: queryResult.data,
+    error: queryResult.error
+  }
+}
+
+async function getMovieId (movieName) {
+  movieName = movieName.trim()
+
+  let movieId = null
+
+  // Check if a movie already exists with the provided name.
+  const queryMoviesResult = await quotesApi.query(
+    gql`
+      query ($movieName: String!) {
+        movies(where: { name: { eq: $movieName } }) {
+          id
+        }
+      }
+    `,
+    { movieName }
+  )
+
+  if (queryMoviesResult.error) {
+    return null
+  }
+
+  const movieExists = queryMoviesResult.data?.movies.length === 1
+  if (movieExists) {
+    movieId = queryMoviesResult.data.movies[0].id
+  } else {
+    // Create a new movie entity record.
+    const saveMovieResult = await quotesApi.mutation(
+      gql`
+        mutation ($movieName: String!) {
+          saveMovie(input: { name: $movieName }) {
+            id
+          }
+        }
+      `,
+      { movieName }
+    )
+
+    if (saveMovieResult.error) {
+      return null
+    }
+
+    movieId = saveMovieResult.data?.saveMovie.id
+  }
+
+  return movieId
+}
+
+export const quotesApi = {
+  async query (gqlQuery, queryVariables = {}) {
+    return await graphqlClientWrapper('query', gqlQuery, queryVariables)
+  },
+  async mutation (gqlQuery, queryVariables = {}) {
+    return await graphqlClientWrapper('mutation', gqlQuery, queryVariables)
+  },
+  getMovieId
+}
+
+export { gql } from '@urql/core'
---
+++ apps/movie-quotes-frontend-htmx/src/scripts/quote-actions.js
@@ -0,0 +1,25 @@
+/* global confirm */
+import { quotesApi, gql } from '../lib/quotes-api.js'
+
+export function confirmDeleteQuote (form) {
+  if (confirm('Are you sure want to delete this quote?')) {
+    form.submit()
+  }
+}
+
+export async function likeQuote (likeQuote) {
+  likeQuote.classList.add('liked')
+  likeQuote.classList.remove('cursor-pointer')
+
+  const id = Number(likeQuote.dataset.quoteId)
+
+  const { data } = await quotesApi.mutation(gql`
+    mutation($id: ID!) {
+      likeQuote(id: $id)
+    }
+  `, { id })
+
+  if (data?.likeQuote) {
+    likeQuote.querySelector('.likes-count').innerText = data.likeQuote
+  }
+}
